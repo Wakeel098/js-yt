41fd54bbdea8e70f7e98221e0fcdc9910d3e3209
Wakeel098 <100693996+Wakeel098@users.noreply.github.com>
2024-01-16 17:08:32 +0000

    xyz

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId="Wakeel098"
+let accountEmail="wakeel@gmail.com"
+var accountPassword=1234
+accountCity="anantnag"
+ 
+// accountId="wakeel123"---------->throws error on consoling after re-assigning:Assignment to constant variable.
+console.log(accountId)
+
+
+accountEmail="xyz@gmail.com" 
+accountPassword=0987
+accountCity="takiya behram shah"
+
+let accountState          //---->>>without initialization :by default undefined
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+
+
+
+
