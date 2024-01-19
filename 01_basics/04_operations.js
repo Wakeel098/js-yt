e4d0d004545cc26a8fa01640ac18ad9a490d3ebb
Wakeel098 <100693996+Wakeel098@users.noreply.github.com>
2024-01-19 12:24:09 +0000

    operators

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,15 @@
+console.log(2+2);  //op:4
+console.log(2-2);  //op:0
+console.log(2*2);  //op:4
+console.log(2**3); //op:8
+console.log(2/3);  //op:0.6666666666666666
+console.log(3%2);  //op:1
+
+// ---->  + is used as string concatianataion in js
+
+// some tricky situations
+console.log(1+"2");      //op:12
+console.log("1"+2);      //op:12
+console.log("1"+2+2);    //op:122
+console.log(1+2+"2");    //op:32
+console.log(1+"2"+2);    //op:122  
