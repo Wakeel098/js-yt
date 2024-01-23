8383914140047c721d667f707f354daf1e4efa7a
Wakeel098 <100693996+Wakeel098@users.noreply.github.com>
2024-01-23 12:22:09 +0000

    number

--- 01_basics/07_number_math.js
+++ 01_basics/07_number_math.js
@@ -13,4 +13,15 @@ console.log(value.toFixed(3))    //op:100.000----->toFixed gives no. precissions
 console.log(value.toPrecision(4))   //op:100.0---->no of precisson values from the begining . if length of number is less than no. of precission values provided then we will get the no. of precission values provided but in exponential form
 
 console.log(value.toPrecision(2))  // output will be in exponential form
+console.log(123.8966.toPrecision(3))   //op:124
 
+ 
+console.log(Number(13424322).toLocaleString())   //toLocalString converts the number into USA standered number system
+console.log(Number(13424322).toLocaleString('en-IN'))   // the number will be converted  as per  indian standered number system
+
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+
+
+
+/***************************************************MATHS********************************************************* */
