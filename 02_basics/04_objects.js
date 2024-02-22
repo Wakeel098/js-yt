0d7db7a9b6461eef8312b8db4e14c472c833991b
Wakeel098 <100693996+Wakeel098@users.noreply.github.com>
2024-02-22 13:58:44 +0000

    objects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,50 @@
+// objects using constructor---singleton object
+
+const user= new Object();
+
+user.id=123;
+user.name="wakeel";
+user.email="kumarwakeel@gmail.com";
+
+console.log(user);
+
+
+//nesting of objects 
+
+const obj={
+    email:"abc@123",
+    fullname:{
+        userFullName:{
+            firstName:"wakeel",
+            lastName:"kumar",
+        }
+    }
+
+}
+
+//accessing nested objects
+
+console.log(obj.fullname?.userFullName.lastName);   //    ? -->used for optional chaining
+
+//merging of objects: 1) by using Object.assign  2) by using spread operator
+
+const obj1={
+    1:"abc",
+    2:"def"
+}
+
+const obj2={
+    3:"geh",
+    4:"ijk"
+}
+
+const m1=Object.assign({},obj1,obj2);
+const m2={...obj1,...obj2}
+console.log(m1,m2)
+
+console.log(Object.keys(obj1));
+console.log(Object.values(obj));
+console.log(Object.entries(obj2));
+
+console.log(obj.hasOwnProperty('key'))
+console.log(obj.hasOwnProperty('email'))
